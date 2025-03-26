b455ee68ccb760c3791eae4229b16e4b18dffde8
Loh Siu Yin <siuyin@beyondbroadcast.com>
2025-03-26 14:38:54 +0800

    feat: add basic map

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+
+	"github.com/siuyin/dflt"
+	"github.com/siuyin/gmap/public"
+)
+
+func main() {
+	http.HandleFunc("/{$}", indexPageHandler)
+	http.Handle("/", http.FileServer(http.FS(public.Content)))
+	http.HandleFunc("/ger", gerHandler)
+
+	port := dflt.EnvString("PORT", "8080")
+	fmt.Printf("Starting server on PORT=%s", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+func indexPageHandler(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFS(public.Content, "index.html"))
+	key := dflt.EnvString("GOOGLE_MAPS_API_KEY", "your-api-key-here")
+	t.ExecuteTemplate(w, "index.html", struct{ Key string }{key})
+}
+
+func gerHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello from /ger")
+}
